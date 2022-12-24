32b514db1b867e091168206bb32c75b8a367f2c0
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-12-23 21:15:41 -0300

    art: spiderman

--- css-arts/src/router/index.js
+++ css-arts/src/router/index.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
+import Spiderman from '@/arts/Spiderman.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Spiderman
     },
     {
       // keeping this block of code to have a example of lazy-loading
